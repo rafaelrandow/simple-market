23a5fe83f79e724a4e74e0b1c288805e5a5b3ef6
Rafael Randow Grossi <rafaelrandow@gmail.com>
2017-11-01 15:08:16 -0200

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,17 +2,29 @@ $(document).ready(function(){
 	//shows menu
  $('.navbar-toggle').on("click", function(e){
     $('#navbar-main').toggle();
+     e.stopPropagation();
+     e.preventDefault();
   });  
+   //dismiss menu
+ $('.container').on("click", function(e){
+	if ($("#navbar-main").css("display") == "block"){
+		$('#navbar-main').toggle();
+	}
+    // e.stopPropagation();
+    //  e.preventDefault();
+  }); 
   
    $('.btnImportar').on("click", function(e){
     $('#listar').hide();
 	$('#limpar').hide();
-	$('#importar').show();
+  $('#importar').show();
+  e.preventDefault();
   });  
   $('.btnLimpar').on("click", function(e){
     $('#listar').hide();
 	$('#importar').hide();
-	$('#limpar').show();
+  $('#limpar').show();  
+  e.preventDefault();
   });  
    $('.btnHome').on("click", function(e){
     $('#listar').show();
