390f4b950a065aebf80f85d9e83653ca2dc7de12
Rafael Randow Grossi <rafaelrandow@gmail.com>
2017-11-01 14:48:05 -0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -2,17 +2,7 @@ $(document).ready(function(){
 	//shows menu
  $('.navbar-toggle').on("click", function(e){
     $('#navbar-main').toggle();
-    e.stopPropagation();
-    e.preventDefault();
   });  
-   //dismiss menu
- $('.container').on("click", function(e){
-	if ($("#navbar-main").css("display") == "block"){
-		$('#navbar-main').toggle();
-	}
-    e.stopPropagation();
-    e.preventDefault();
-  }); 
   
    $('.btnImportar').on("click", function(e){
     $('#listar').hide();
